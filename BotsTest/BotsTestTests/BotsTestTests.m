a9147ea9d8bb8c7269acf34de22c3c848ffcb017
Trinh Tran <trinhduc20@gmail.com>
2014-04-03 14:48:01 +0700

    FIrst

---
+++ BotsTest/BotsTestTests/BotsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  BotsTestTests.m
+//  BotsTestTests
+//
+//  Created by TMS  on 4/3/14.
+//  Copyright (c) 2014 Jony Tran. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BotsTestTests : XCTestCase
+
+@end
+
+@implementation BotsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
